2a7124a15b344113e349bf90d4f250bbc817f22c
madsgit0 <landolfomattia@gmail.com>
2022-11-29 15:36:20 +0100

    setting

---
+++ notbored/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SavedComponent } from './pages/saved/saved.component';
+import { DoneComponent } from './pages/done/done.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GenRandomComponent } from './components/gen-random/gen-random.component';
+import { GenTypeComponent } from './components/gen-type/gen-type.component';
+import { GenBudgetComponent } from './components/gen-budget/gen-budget.component';
+import { GenEasinessComponent } from './components/gen-easiness/gen-easiness.component';
+import { GenGroupComponent } from './components/gen-group/gen-group.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { ListCardComponent } from './components/list-card/list-card.component';
+
+
+@NgModule({
+  declarations: [
+
+  
+    HomeComponent,
+        SavedComponent,
+        DoneComponent,
+        NavbarComponent,
+        GenRandomComponent,
+        GenTypeComponent,
+        GenBudgetComponent,
+        GenEasinessComponent,
+        GenGroupComponent,
+        HeaderComponent,
+        CardComponent,
+        ListCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
